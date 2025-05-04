0b3994a7cb86a0d8965b51995538aa343dc398a0
Michael <michaelbalogun34@gmail.com>
2025-05-04 20:37:01 +0100

    feat: update service images to local assets for improved loading performance and consistency

--- src/pages/Home/our services/index.jsx
+++ src/pages/Home/our services/index.jsx
@@ -1,61 +1,74 @@
 import React from "react";
 import ServicesSection from './services/servicesSection';
+import waecResultChecker from '../../../images/waec-result-checker.jpg';
+import necoResultChecker from '../../../images/neco-result-checker.jpg';
+import nabtebResultChecker from '../../../images/nabteb-result-checker.jpg';
+import nbaisResultChecker from '../../../images/nbais-result-checker.jpg';
+import bvn from '../../../images/bvn.jpg';
+import waecGce from '../../../images/waec-gce.jpg';
+import nin from '../../../images/nin.jpg';
+import dataSubscription from '../../../images/data-subscription.jpg';
+import olevelUpload from '../../../images/olevel-upload.jpg';
+import { useState } from "react";
+import { useEffect } from "react";
+import { data, useNavigate } from "react-router-dom";
+
 
 // Service data
 const servicesData = [
   {
     id: 1,
     title: "WAEC Result Checker",
-    image: "https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?auto=format&fit=crop&w=500&h=300",
+    image: waecResultChecker,
     price: 3400,
     category: "Exam Scratch Cards",
   },
   {
     id: 2,
     title: "NECO Result Checker",
-    image: "https://images.unsplash.com/photo-1461749280684-dccba630e2f6?auto=format&fit=crop&w=500&h=300",
+    image: necoResultChecker,
     price: 1300,
     category: "Exam Scratch Cards",
   },
   {
     id: 3,
     title: "NABTEB Result Checker",
-    image: "https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?auto=format&fit=crop&w=500&h=300",
+    image: nabtebResultChecker,
     price: 900,
     category: "Exam Scratch Cards",
   },
   {
     id: 4,
     title: "WAEC Verification",
-    image: "https://images.unsplash.com/photo-1581091226825-a6a2a5aee158?auto=format&fit=crop&w=500&h=300",
+    image: waecGce,
     price: 5000,
     category: "Other Services",
   },
   {
     id: 5,
     title: "NIN Registration",
-    image: "https://images.unsplash.com/photo-1649972904349-6e44c42644a7?auto=format&fit=crop&w=500&h=300",
+    image: nin,
     price: 2000,
     category: "Other Services",
   },
   {
     id: 6,
     title: "BVN Verification",
-    image: "https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?auto=format&fit=crop&w=500&h=300",
+    image: bvn,
     price: 1500,
     category: "Other Services",
   },
   {
     id: 7,
     title: "Data Subscription",
-    image: "https://images.unsplash.com/photo-1461749280684-dccba630e2f6?auto=format&fit=crop&w=500&h=300",
+    image: dataSubscription,
     price: 200,
     category: "Data Subscription",
   },
   {
     id: 8,
     title: "O-Level Upload",
-    image: "https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?auto=format&fit=crop&w=500&h=300",
+    image: olevelUpload,
     price: 1000,
     category: "Other Services",
   },
